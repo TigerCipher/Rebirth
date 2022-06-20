43acc2275001ad7b02ca9186bf271e7e1aaa7c96
TigerCipher <matt.rogers8396@gmail.com>
2022-06-20 00:02:44 -0500

    opengl error callback

--- Rebirth/src/platform/opengl/OpenGLContext.cpp
+++ Rebirth/src/platform/opengl/OpenGLContext.cpp
@@ -30,6 +30,11 @@
 
 #include <glad/glad.h>
 
+void GLAPIENTRY GLErrorCallback(GLenum src, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	RB_CORE_ERROR("OpenGL Error (Severity: {}): {} (type = {}) -> {}", severity, (type == GL_DEBUG_TYPE_ERROR ? "*GL ERROR*" : ""), type, message);
+}
+
 rebirth::OpenGLContext::OpenGLContext(GLFWwindow* window): mWindow(window)
 {
 	RB_CORE_ASSERT(window, "Window handle is nullptr");
@@ -50,6 +55,11 @@ void rebirth::OpenGLContext::Init()
 	const char* glVersionName = (char*)glGetString(GL_VERSION);
 	RB_CORE_INFO("Detected graphics unit: {} ({})", rendererName, vendorName);
 	RB_CORE_INFO("Available OpenGL Version: {}", glVersionName);
+
+	#ifdef RB_DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(GLErrorCallback, nullptr);
+	#endif
 
 	RB_CORE_TRACE("OpenGL Context created");
 }
