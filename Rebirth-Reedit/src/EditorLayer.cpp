74728a119b1749892e639f3e8e9a9fb4c6bdea16
TigerCipher <matt.rogers8396@gmail.com>
2022-07-12 20:38:32 -0500

    testing the branch

--- Rebirth-Reedit/src/EditorLayer.cpp
+++ Rebirth-Reedit/src/EditorLayer.cpp
@@ -157,6 +157,7 @@ namespace rebirth
 		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
 		{
 			int pixelData = mFramebuffer->ReadPixel(1, mouseX, mouseY);
+			// testing mousepicking branch
 			RB_CORE_WARN("Pixel Data: {}", pixelData);
 		}
 
